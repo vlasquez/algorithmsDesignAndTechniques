7802ed64ab5060c96105bf5a37cf94d4a9c1aca9
Andres Velasquez <VLASQUEZ53069@HOTMAIL.COM>
2018-10-07 11:01:27 -0500

    MaxPairwiseProduct

--- src/MaxPairwaseNumber.java
+++
@@ -1,6 +0,0 @@
-/**
- * @autor Andrés Velasquez
- * @since 7/10/18
- **/
-public class MaxPairwaseNumber {
-}
---
+++ src/MaxPairwiseNumber.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * @autor Andrés Velasquez
+ * @since 7/10/18
+ **/
+public class MaxPairwiseNumber {
+  public static void main(String [] args)
+  {
+    FastScanner scanner = new FastScanner(System.in);
+    int n = scanner.nextInt();
+    int[] numbers = new int[n];
+    for (int i = 0; i < n; i++) {
+      numbers[i] = scanner.nextInt();
+    }
+    System.out.println(getMaxPairwiseProduct(numbers));
+  }
+
+  static int getMaxPairwiseProduct(int[] numbers){
+    int product = 0;
+    int n = numbers.length;
+
+    for (int i = 0; i<n; ++i ){
+      for(int j = i +1; j<n; ++j){
+          product = Math.max(product,numbers[i]*numbers[j]);
+      }
+    }
+    return product;
+  }
+  static class FastScanner {
+    BufferedReader br;
+    StringTokenizer st;
+
+    FastScanner(InputStream stream) {
+      try {
+        br = new BufferedReader(new
+            InputStreamReader(stream));
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    String next() {
+      while (st == null || !st.hasMoreTokens()) {
+        try {st = new StringTokenizer(br.readLine());
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+      return st.nextToken();
+    }
+    int nextInt() {
+      return Integer.parseInt(next());
+    }
+  }
+
+}
